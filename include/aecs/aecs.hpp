13a70d179367b9b95f52e6d97b320f2ed045887d
N-Adkins <noahcadkins@gmail.com>
2024-03-19 22:38:15 -0400

    Revert malformed constructors

--- include/aecs/aecs.hpp
+++ include/aecs/aecs.hpp
@@ -123,10 +123,6 @@ std::size_t get_component_id()
 template <typename entity_id>
 struct i_component_allocator
 {
-    i_component_allocator(const i_component_allocator&) = default;
-    i_component_allocator(i_component_allocator&&) = default;
-    i_component_allocator& operator=(const i_component_allocator&) = default;
-    i_component_allocator& operator=(i_component_allocator&&) = default;
     virtual ~i_component_allocator() = default;
     virtual void entity_destroyed(entity_id entity) = 0;
 };
